d95546cd9c7a9be135e9bdbd3050b3767ce65512
Syuko4omi <riorioblackcat@yahoo.co.jp>
2023-08-10 20:07:06 +0900

    ✨ implement new method change by one hiragana

--- src/change_by_homonym.py
+++ src/change_by_homonym.py
@@ -1,6 +1,18 @@
 import urllib.request
 import urllib.parse
 import ast
+import jaconv
+import random
+from config import KANJI_IDIOM_REGEX
+import re
+
+
+def is_kanji_idiom(idiom: str) -> bool:
+    flag = False
+    if re.match(KANJI_IDIOM_REGEX, idiom) is not None:
+        if re.match(KANJI_IDIOM_REGEX, idiom).group() == idiom:
+            flag = True
+    return flag
 
 
 def suggest_homonym_list(hiragana_form: str, original_kanji_form: str) -> list[str]:
@@ -30,10 +42,39 @@ def suggest_homonym_list(hiragana_form: str, original_kanji_form: str) -> list[s
             "utf-8"
         )  # return should be one-line string
         homonym_list = ast.literal_eval(ret_string)  # convert str to list
-        homonym_list_excluded_original_form = [
-            item for item in homonym_list[0][1] if item != original_kanji_form
-        ]
+        if len(homonym_list) == 1:
+            homonym_list_excluded_original_form = [
+                item for item in homonym_list[0][1] if item != original_kanji_form
+            ]
+        else:  # those don't have any homonym or separated idioms are excluded
+            homonym_list_excluded_original_form = []
         return homonym_list_excluded_original_form
+
+
+def generate_goji_tweet_hom(surface, reading, noun_pos_shuffled):
+    replaced_pos_id = 0
+    mistaken_idiom = ""
+    for noun_pos in noun_pos_shuffled:
+        goji_candidate = suggest_homonym_list(
+            hiragana_form=jaconv.kata2hira(reading[noun_pos]),
+            original_kanji_form=surface[noun_pos],
+        )
+        if len(goji_candidate) != 0:
+            goji_candidate = [
+                candidate
+                for candidate in goji_candidate
+                if is_kanji_idiom(candidate) is True
+            ]
+            replaced_pos_id = noun_pos
+            mistaken_idiom = random.choice(goji_candidate)
+            break
+    after_tweet = ""
+    for idx, item in enumerate(surface):
+        if idx != replaced_pos_id:
+            after_tweet += item
+        else:
+            after_tweet += mistaken_idiom
+    return after_tweet
 
 
 """
--- src/create_goji_tweet.py
+++ src/create_goji_tweet.py
@@ -1,9 +1,9 @@
-from change_by_homonym import suggest_homonym_list
 from janome.tokenizer import Tokenizer
 import random
 import re
-import jaconv
 from config import KANJI_IDIOM_REGEX
+from change_by_homonym import generate_goji_tweet_hom
+from change_by_one_hiragana import generate_goji_tweet_one_hira
 
 
 def is_kanji_idiom(idiom: str) -> bool:
@@ -32,36 +32,16 @@ def tweet_preprocesser(original_tweet: str):
 def goji_tweet_generator(original_tweet: str, goji_type: str) -> str:
     surface, reading, candidate_noun_pos = tweet_preprocesser(original_tweet)
     noun_pos_shuffled = random.sample(candidate_noun_pos, len(candidate_noun_pos))
-    replaced_pos_id = 0
-    mistaken_idiom = ""
-    for noun_pos in noun_pos_shuffled:
-        if goji_type == "homonym":
-            goji_candidate = suggest_homonym_list(
-                hiragana_form=jaconv.kata2hira(reading[noun_pos]),
-                original_kanji_form=surface[noun_pos],
-            )
-            if len(goji_candidate) != 0:
-                print(goji_candidate)
-                goji_candidate = [
-                    candidate
-                    for candidate in goji_candidate
-                    if is_kanji_idiom(candidate) is True
-                ]
-                print(goji_candidate)
-                replaced_pos_id = noun_pos
-                mistaken_idiom = random.choice(goji_candidate)
-                break
-    after_tweet = ""
-    for idx, item in enumerate(surface):
-        if idx != replaced_pos_id:
-            after_tweet += item
-        else:
-            after_tweet += mistaken_idiom
+    if goji_type == "homonym":
+        after_tweet = generate_goji_tweet_hom(surface, reading, noun_pos_shuffled)
+    else:
+        after_tweet = generate_goji_tweet_one_hira(surface, reading, noun_pos_shuffled)
     return after_tweet
 
 
 if __name__ == "__main__":
     tokenizer = Tokenizer()
     original_tweet = "きええええぇえええ。。\n後30分でお出掛けなのに書類が散乱してるよぉおお😭😭😭"
-    goji_tweet = goji_tweet_generator(original_tweet, "homonym")
+    # goji_tweet = goji_tweet_generator(original_tweet, "homonym")
+    goji_tweet = goji_tweet_generator(original_tweet, "hiragana")
     print(goji_tweet)
